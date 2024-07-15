585aace1a2b3e0987a817654c28a7e1227f41861
Mervin Diaz <mervindiazlugo@gmail.com>
2024-07-15 14:51:23 -0700

    clase 44-46

--- src/test/java/myProject/runners/web/ExamplesWebRunner.java
+++ src/test/java/myProject/runners/web/ExamplesWebRunner.java
@@ -9,7 +9,7 @@ import io.cucumber.testng.CucumberOptions;
         plugin = {
                 "pretty",
                 "html:test-output",
-                "json:target/cucumber/cucumber.json",
+                "json:target/cucumber/examples-cucumber.json",
                 "html:target/cucumber-html-report.html"
         })
 public class ExamplesWebRunner extends AbstractTestNGCucumberTests{
